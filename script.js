cd958f6bfb532c8f762c4d4d4c39fd874a3b8a76
Jewgienij Walczykowski <jewgienij.walczykowski@gmail.com>
2023-03-14 15:23:32 +0100

    New moving info circle on the main page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// alert("Welcome to my Dev Portfolio! 😊")
+// Rotation circle with logo.
+// (i * 360)/textLong}deg - auto letters spread
+
+const circleInfo = document.querySelector(".circleText");
+const textLong = circleInfo.innerText.length;
+
+circleInfo.innerHTML = circleInfo.innerText
+  .split("")
+  .map(
+    (char, i) =>
+      `<span style = "transform:rotate(${
+        (i * 360) / textLong
+      }deg)">${char}</span>`
+  )
+  .join("");
